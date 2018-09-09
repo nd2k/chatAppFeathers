e587ed056160871e7ad26443b2daceef44b4aceb
Nicolas Decat <nicolas.decat@gmail.com>
2018-09-09 02:02:19 +0200

    app is fully working

---
+++ test/hooks/gravitar.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const feathers = require('@feathersjs/feathers');
+const gravitar = require('../../src/hooks/gravitar');
+
+describe('\'gravitar\' hook', () => {
+  let app;
+
+  beforeEach(() => {
+    app = feathers();
+
+    app.use('/dummy', {
+      async get(id) {
+        return { id };
+      }
+    });
+
+    app.service('dummy').hooks({
+      before: gravitar()
+    });
+  });
+
+  it('runs the hook', async () => {
+    const result = await app.service('dummy').get('test');
+    
+    assert.deepEqual(result, { id: 'test' });
+  });
+});
